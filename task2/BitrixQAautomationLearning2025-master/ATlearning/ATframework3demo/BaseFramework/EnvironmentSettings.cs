48e2d90613e466df1c8615e987928d7e786542fe
Elbeg <dlyaorigina2002@mail.ru>
2025-01-25 01:27:00 +0200

    delete

--- task2/BitrixQAautomationLearning2025-master/ATlearning/ATframework3demo/BaseFramework/EnvironmentSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ATframework3demo.BaseFramework
-{
-    public class EnvironmentSettings
-    {
-        public static List<string> AppArgs { get; set; } = new List<string>();
-        public static bool IsDebug => AppArgs.Contains("debug");
-    }
-}
